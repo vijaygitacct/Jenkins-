2305bf4b2240a51054b5d79182b30362328febc5
vijaygitacct <114874733+vijaygitacct@users.noreply.github.com>
2023-07-11 12:33:31 +0530

    Update JenkinsSample.java

--- JenkinsSample.java
+++ JenkinsSample.java
@@ -1,11 +1,5 @@
 package SampleAbstract;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
 public class JenkinsSample {
 	public static void main(String[] args) {
